9a4f446ed3332cb356fcaaa321a6d21859957938
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-08-30 15:47:00 +0530

    Added Api Service

--- apiService.js
+++ apiService.js
@@ -1,24 +1,50 @@
-
-//module.exports = function apiClass () {
-
+module.exports = function apiClass () {
+this.ApiRequest = function (userSays) 
+{
 var apiai = require('apiai');
-var app =  apiai("Bearer 2236694c3ac943ce93d21afe990b841d");
-queryProcess("hi");
-function queryProcess(query){
-  var request = app.textRequest('<Your text query>', {
-    sessionId: '<unique session id>'
+var app =  apiai("2236694c3ac943ce93d21afe990b841d");
+var request = app.textRequest(userSays, {
+sessionId: '0111'
 });
- 
+
 request.on('response', function(response) {
     result=response.result.fulfillment.speech;
-    console.log(result);
-    console.log(response);
 });
- 
+
 request.on('error', function(error) {
-    console.log(error);
+console.log(error);
 });
-}
+request.end();
+
+
+
+}}
+
+
+
+
+
+// module.exports = function apiClass () {
+
+// var apiai = require('apiai');
+// var app =  apiai("Bearer 2236694c3ac943ce93d21afe990b841d");
+
+// function queryProcess(query){
+//   var request = app.textRequest('query', {
+//     sessionId: '<unique session id>'
+// });
+ 
+// request.on('response', function(response) {
+//     result=response.result.fulfillment.speech;
+//     console.log(result);
+//     console.log(response);
+// });
+ 
+// request.on('error', function(error) {
+//     console.log(error);
+// });
+// }
+// }
 
 
 
--- main.js
+++ main.js
@@ -3,8 +3,7 @@ const app =electron.app
 const BrowserWindow = electron.BrowserWindow
 const path = require('path')
 const url = require('url')
-const apiService=require('./apiService')
-console.log(apiService.apiClass());
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win
