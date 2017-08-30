9304171f20d1c444c66ae4b4761e653dd88149a0
VigneshwaranR-Hexaware <vigneshwaranr.hexaware@gmail.com>
2017-08-30 18:33:03 +0530

    Updated index.js

--- apiService.js
+++ apiService.js
@@ -2,13 +2,14 @@ module.exports = function apiClass () {
 this.ApiRequest = function (userSays) 
 {
 var apiai = require('apiai');
-var app =  apiai("2236694c3ac943ce93d21afe990b841d");
+var app =  apiai("84a42b482cf74b9c9914495c757de603");
 var request = app.textRequest(userSays, {
 sessionId: '0111'
 });
 
 request.on('response', function(response) {
     result=response.result.fulfillment.speech;
+    insertChat("you", result);  
 });
 
 request.on('error', function(error) {
