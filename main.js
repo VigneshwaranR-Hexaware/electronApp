296a0912619d8cb004c7bcc3e8c69d0cc3167dd0
hexinnov <htlp001@gmail.com>
2017-08-31 16:43:54 +0530

    updated main.js

--- main.js
+++ main.js
@@ -6,13 +6,9 @@ const path = require('path')
 const url = require('url')
 const iconPath = path.join(__dirname, './vfs-global.jpg');
 
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
 let win
 
 function createWindow () {
-  // Create the browser window.
   const trayControl = new appTray(iconPath)
   trayControl.on('click',function(){
     if(win){
@@ -39,24 +35,12 @@ function createWindow () {
   // Open the DevTools.
   win.webContents.openDevTools()
   win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
     win = null
   })
-
 })
-
-  // and load the index.html of the app.
-  
-
-  // Emitted when the window is closed.
-  
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
+
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
@@ -78,8 +62,6 @@ app.on('window-all-closed', () => {
 
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
   if (win === null) {
     createWindow()
   }
