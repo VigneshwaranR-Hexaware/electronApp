67d12c4e42a422dd94736cf7bc137a408f227c7b
hexinnov <htlp001@gmail.com>
2017-09-14 15:09:30 +0530

    Updated appconfig

--- appConfig.js
+++ appConfig.js
@@ -2,7 +2,9 @@ module.exports.appConfig = {
     _DeveloperAccessToken:"VFS",
     _FromName : "Test Function",
     _ToName:"BOT",
-    
+    _FromImage:"",
+    _ToImage:"",
+    _FilePath:""
    };
    
   
