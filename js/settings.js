227a17f1ba36fee4f8a8440d5bd1184212631607
Pourab Karchaudhuri <36023@Hexaware.com>
2017-10-16 19:49:01 +0530

    Sync

--- js/settings.js
+++ js/settings.js
@@ -10,7 +10,6 @@ define({
 
     accessToken: "6c7e02249ef94cc68b647881b36f0739",
     chatServerURL : "https://api.api.ai/v1/",
-
     applicationTitle: "RICOH Assistant",
     userTitle : "Me",
     botTitle : "RICOH Assistant",
