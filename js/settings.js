3dd8fdf4b33f6e5028c67aad4e6ae9604f48c1f1
Pourab Karchaudhuri <36023@Hexaware.com>
2017-10-16 19:31:48 +0530

    Rolled Back Stability

--- js/settings.js
+++ js/settings.js
@@ -8,7 +8,7 @@ This file is part of the Innovation LAB - Offline Bot.
 
 define({
 
-    accessToken: "0bd6324ba1b64892ab2d68864808285c",
+    accessToken: "6c7e02249ef94cc68b647881b36f0739",
     chatServerURL : "https://api.api.ai/v1/",
 
     applicationTitle: "RICOH Assistant",
